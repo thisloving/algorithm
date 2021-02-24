ae621a9603456ef26bdeb614b067c92364e9ef00
thisloving <thisloving@gmail.com>
2021-02-24 10:44:20 +0800

    quick sort

---
+++ sort/quick_sort/quick_sort.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func quickSort(data []int) []int {
+	if len(data) <= 1 {
+		return data
+	}
+
+	var wg sync.WaitGroup
+
+	c := data[0]
+	var s1, s2 []int
+
+	for k, v := range data {
+		if k == 0 {
+			continue
+		}
+
+		if v > c {
+			s2 = append(s2, v)
+		} else {
+			s1 = append(s1, v)
+		}
+	}
+
+	wg.Add(2)
+
+	go func() {
+		s1 = quickSort(s1)
+		wg.Done()
+	}()
+
+	go func() {
+		s2 = quickSort(s2)
+		wg.Done()
+	}()
+
+	wg.Wait()
+
+	data = []int{c}
+	if len(s1) > 0 {
+		data = append(s1, data...)
+	}
+
+	if len(s2) > 0 {
+		data = append(data, s2...)
+	}
+
+	return data
+}
+
+func main() {
+	data := []int{3, 6, 23, 7, 2, 4, 9, 13}
+	fmt.Println(quickSort(data))
+}
