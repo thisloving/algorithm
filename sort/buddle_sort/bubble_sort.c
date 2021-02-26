a8fd19580f24fa978af3862c7097189d88f815f4
thisloving <thisloving@gmail.com>
2021-02-26 15:27:26 +0800

    bubble sort

--- sort/buddle_sort/bubble_sort.c
+++ sort/buddle_sort/bubble_sort.c
@@ -17,7 +17,7 @@ void bubbleSort(int arr[], int n) {
 	for (i = 0; i < n; i++) { // for (i = 0; i < n-1; i++) {
 		swapped = false;
 		for (j = 0; j < n-i-1; j++) {
-			if (arr[j] < arr[j+1]) {
+			if (arr[j] > arr[j+1]) {
 				swap(&arr[j], &arr[j+1]);
 				swapped = true;
 			}
