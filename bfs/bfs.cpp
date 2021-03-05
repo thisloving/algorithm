d409c5b8cc2e7e6078c4aff37db17ea9910a13bd
thisloving <thisloving@gmail.com>
2021-03-05 15:04:57 +0800

    bfs

--- bfs/bfs.cpp
+++ bfs/bfs.cpp
@@ -30,22 +30,46 @@ void Graph::AddVertex(int parent, int child) {
 }
 
 void Graph::Initialize() {
+#if 0
+	// tree
 	this->AddVertex(0, 1);
 	this->AddVertex(0, 2);
 	this->AddVertex(1, 3);
 	this->AddVertex(1, 4);
 	this->AddVertex(2, 5);
 	this->AddVertex(2, 6);
+#endif
+	
+#if 1
+	// undirected graph
+	this->AddVertex(0, 1);
+	this->AddVertex(0, 2);
+	this->AddVertex(1, 0);
+	this->AddVertex(1, 3);
+	this->AddVertex(2, 0);
+	this->AddVertex(2, 3);
+	this->AddVertex(3, 1);
+	this->AddVertex(3, 2);
+	this->AddVertex(3, 4);
+	this->AddVertex(3, 5);
+	this->AddVertex(4, 3);
+	this->AddVertex(4, 6);
+	this->AddVertex(5, 3);
+	this->AddVertex(5, 6);
+	this->AddVertex(6, 4);
+	this->AddVertex(6, 5);
+#endif
 
 	std::queue<int>().swap(this->q);
 	this->visited.clear();
 }
 
 void Graph::BFS(int vertex) {
-	if (this->visited.find(vertex) != this->visited.end()) {
+	if (vertex+1 > kVertexNum) {
 		return;
 	}
 
+	this->visited.insert(vertex);
 	this->q.push(vertex);
 
 	while(!this->q.empty()) {
@@ -57,6 +81,9 @@ void Graph::BFS(int vertex) {
 		if (iter1st != this->vertex.end()) {
 			std::vector<int>::iterator iter2nd = iter1st->second.begin();
 			for (; iter2nd != iter1st->second.end(); ++iter2nd) {
+				if (this->visited.find(*iter2nd) != this->visited.end()) {
+					continue;
+				}
 				this->visited.insert(*iter2nd);
 				this->q.push(*iter2nd);
 			}
@@ -70,7 +97,5 @@ int main()
 	Graph graph;
 	graph.Initialize();
 
-	for (int i = 0; i < kVertexNum; i++) {
-		graph.BFS(i);
-	}
+	graph.BFS(0);
 }
