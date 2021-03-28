8314b33275a10dc1f6c8496d26593afb589fa4f2
thisloving <thisloving@gmail.com>
2021-03-28 18:33:52 +0800

    unique paths with obstacles

---
+++ dynamic_programming/unique_paths_with_obstacles/unique_paths_with_obstacles.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+
+func uniquePathsWithObstacles(array [][]int, m int, n int) int {
+	if m == 0 || n == 0 {
+		return 0
+	}
+
+	dp := make([][]int, m)
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+		for j := 0; j < n; j++ {
+			dp[i][j] = 0
+		}
+	}
+
+	dp[0][0] = 1
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if array[j][i] == 1 {
+				dp[i][j] = 0
+			} else {
+				if i == 0 || j == 0 {
+					dp[i][j] = 1
+				} else {
+					dp[i][j] = 0
+					if i-1 >= 0 {
+						dp[i][j] += dp[i-1][j]
+					}
+
+					if j-1 >= 0 {
+						dp[i][j] += dp[i][j-1]
+					}
+				}
+			}
+		}
+	}
+
+	return dp[m-1][n-1]
+}
+
+func main() {
+	array := [][]int{
+		{0, 0, 0, 1, 0, 7},
+		{0, 0, 1, 0, 0, 0},
+		{0, 0, 0, 1, 0, 0},
+		{0, 0, 0, 0, 1, 0},
+		{0, 0, 0, 0, 0, 0},
+	}
+
+	fmt.Println(uniquePathsWithObstacles(array, 6, 5))
+}
